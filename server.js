a9f1def704017c130f7f0a6c8eb5e4ac9c1fafb7
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-05-19 19:00:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(errorHandler);
 app.use(notFound);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`));

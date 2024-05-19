47cc517c2763aeea793a174ab6fac118ab697574
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-05-19 19:08:58 +0300

    йцу

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,13 +1,14 @@
-const notFound = async (req, res, next) => {
-  const error = new Error(`Not Found ${req.originalUrl}`);
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
-  res.json({ success: false, message: err.message });
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
 };
-
-export { notFound, errorHandler };
